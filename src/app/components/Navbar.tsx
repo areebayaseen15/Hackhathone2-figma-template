d436d0b3e5c3352f17d5f324b4e1874c4490214a
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-12-10 01:39:14 +0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -12,20 +12,20 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 bg-white shadow-md z-50 ">
-      <div className="px-4 md:px-6 py-4">
-        <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+    <div className="  fixed top-0 left-0 right-0 bg-white shadow-md z-50  ">
+      <div className="  w-[375px] h-[150px] lg:mx-w-[1440px] lg:mx-h-[124px] px-5 lg:px-10 md:px-6 md:py-10 lg:py-10 ">
+        <div className="flex flex-col gap-0 md:flex-row justify-between items-center space-y-4 md:space-y-0">
           {/* Logo */}
           <div className="flex justify-between items-center w-full md:w-auto">
             <h1
-              className={`text-[24px] md:text-[32px] leading-[48px] tracking-[-3%] font-extrabold text-blue-500`}
+              className={`lg:ml-[50px] text-[24px] md:text-[32px] leading-[48px] tracking-[-3%] font-extrabold text-blue-500`}
             >
               MORENT
             </h1>
           </div>
 
           {/* Search Bar */}
-          <div className="relative gap-8 mr-20 md:mr-10 lg:0  items-center flex justify-between text-[#596780] bg-gray-100 px-3 py-2 w-[263px]  md:w-[492px] h-[48px] rounded-md">
+          <div className="relative ml-40 flex items-center gap-4  md:mr-10 mr-40    md:w-[492px] h-[48px] bg-gray-100 px-3 py-2 rounded-md">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5 text-gray-400"
@@ -41,7 +41,7 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Search something here"
-              className="bg-transparent w-full text-sm outline-none"
+              className="bg-transparent w-[200px]  lg:w-[400px] text-sm outline-none"
             />
             <Image
               src="/Assets/filter.png"
@@ -51,24 +51,40 @@ const Navbar = () => {
               className="absolute right-3 top-1/2 transform -translate-y-1/2"
             />
           </div>
+{/* mobile image */}
+<Link href="/Dashboard">
+
+<Image
+  src="/Assets/img1.png"
+  alt="Profile"
+  width={44}
+  height={44}
+  className="relative bottom-[120px] left-20 rounded-full md:hidden"
+/>
+</Link>
 
           {/* Profile & Notification */}
-          <div className=" hidden md:flex items-center space-x-6">
+          <div className="hidden md:flex items-center lg:justify-center ml-[280px] space-x-3  ">
             <Link href="#">
-              <FaHeart className="text-gray-500 w-[24px] h-[24px] " />
+              <FaHeart className="text-gray-500 w-[24px] h-[24px] mr-5" />
             </Link>
             <Link href="#">
-              <AiFillBell className="text-gray-500 w-[24px] h-[24px] " />
+              <AiFillBell className="text-gray-500 w-[24px] h-[24px]" />
             </Link>
-            <IoIosSettings className="text-gray-500 w-[24px] h-[24px]" />
+            <IoIosSettings className="text-gray-500 w-[100px] h-[24px]" />
 
+            <Link href="/Dashboard">
             <Image
               src="/Assets/img1.png"
               alt="Profile"
-              width={44}
-              height={44}
+              width={60}
+              height={60}
+              className="rounded-full"
             />
+              </Link>
           </div>
+
+         
         </div>
       </div>
     </div>
