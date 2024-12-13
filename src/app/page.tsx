1d445a8f41b58f84f170c5a4ebddfc3af76fab87
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-12-13 07:16:34 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
-import React from 'react'
-// import Navbar from './components/Navbar'
- import Hero from './components/hero'
+import React from "react";
+import Hero from "./components/hero";
 
 const Home = () => {
   return (
-    <div className='bg-[#F6F7F9] px-2 lg:px-32'>
-      <div className=' mt-36'>
-              <Hero />
-       <hr/>
-
-    </div>
-      <div>
+    <div>
+      <div className="bg-[#F6F7F9]  ">
+        <Hero />
       </div>
-
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
